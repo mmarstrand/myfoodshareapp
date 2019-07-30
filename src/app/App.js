286b87c17fe829bad7fdfb4ef9a948794238709c
Maria Marstrand <maria.marstrand@gmail.com>
2019-07-30 10:58:09 +0200

    add route in app

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Cards from "../pages/Cards";
 import Header from "../components/Header";
+import CreateCard from "../components/Card";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Switch>
           <Route path="/marketplace" exact component={Cards} />
+          <Route path="/share" exact component={CreateCard} />
         </Switch>
       </Router>
     </>
