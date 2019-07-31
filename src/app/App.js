24eec3b2621300223650593f35ed0a6555ca7550
Maria Marstrand <maria.marstrand@gmail.com>
2019-07-31 15:51:22 +0200

    Add input state and render in app

--- src/app/App.js
+++ src/app/App.js
@@ -4,16 +4,30 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Cards from "../pages/Cards";
 import Header from "../components/Header";
 import CreateCard from "../pages/CreateCard";
+import cardsMockData from "../pages/__mock__/cards.json";
 
 function App() {
+  const [inputData, setInputData] = React.useState(cardsMockData);
+  console.log(inputData, "newState");
+
+  function handleCreate(card) {
+    setInputData([card, ...inputData]);
+  }
+
   return (
     <>
       <Router>
         <GlobalStyles />
         <Header />
         <Switch>
-          <Route path="/marketplace" exact component={Cards} />
-          <Route path="/share" exact component={CreateCard} />
+          <Route
+            path="/marketplace"
+            render={props => <Cards inputData={inputData} {...props} />}
+          />
+          <Route
+            path="/share"
+            render={props => <CreateCard onCreate={handleCreate} {...props} />}
+          />
         </Switch>
       </Router>
     </>
