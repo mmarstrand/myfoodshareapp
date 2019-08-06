a07ab3f7f44053e3179559602492eff48409ff30
Maria Marstrand <maria.marstrand@gmail.com>
2019-08-06 11:12:24 +0200

    add home buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 const StyledBackground = styled.img`
   position: absolute;
   z-index: -1;
   width: 100vw;
   height: 100vh;
-  filter: grayscale(0.4) saturate(2) brightness(1);
+  filter: grayscale(0.3) saturate(2) brightness(1);
 `;
 
 const StyledHomeButton = styled(Button)`
-  align-items: center;
-  justify-content: center;
+  height: 40px;
+  font-size: 16px;
 `;
 
 const StyledFullscreen = styled.div`
@@ -23,12 +24,23 @@ const StyledFullscreen = styled.div`
   justify-content: center;
 `;
 
+const StyledText = styled.div`
+  color: white;
+  margin: 8px;
+`;
+
 function Home() {
   return (
     <>
       <StyledBackground src="/background.jpg" alt="Food" />
       <StyledFullscreen>
-        <StyledHomeButton>TestBtn</StyledHomeButton>
+        <Link to="/share">
+          <StyledHomeButton>Share food</StyledHomeButton>
+        </Link>
+        <StyledText>or</StyledText>
+        <Link to="/marketplace">
+          <StyledHomeButton>Find food</StyledHomeButton>
+        </Link>
       </StyledFullscreen>
     </>
   );
